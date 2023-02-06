62574337a1549da725a1dceab200f022b93dffdb
madtro <anna.kozitzyna@yandex.ru>
2023-02-06 22:06:07 +0300

    2

--- src/Homework10/Author.java
+++ src/Homework10/Author.java
@@ -7,4 +7,10 @@ public class Author {
         this.name = name;
         this.surname = surname;
     }
+    public String getName(){
+        return this.name;
+    }
+    public String getSurname(){
+        return this.surname;
+    }
 }
--- src/Homework10/Main.java
+++ src/Homework10/Main.java
@@ -4,9 +4,16 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Author GriboedovAS = new Author("Fktrcfylh CСергеевич", "Грибоедов");
-        Book book1 = new Book("Горе от ума", 1970, GriboedovAS);
-        System.out.println("book1.title = " + book1.title);
-        System.out.println("book1.author = " + book1.author);
+        Author griboedovAS = new Author("Александр Сергеевич", "Грибоедов");
+        Author tolstoyLN = new Author("Лев Николаевич", "Толстой");
+        Book book1 = new Book("Горе от ума", 1970, "Грибоедов АС");
+        Book book2 = new Book("Война и мир", 2073, "Толстой ЛН");
+        System.out.println("book1.title = " + book1.getTitle());
+        System.out.println("book1.author = " + book1.getAuthor());
+        System.out.println("book2.title = " + book2.getTitle());
+        System.out.println("book2.author = " + book2.getAuthor());
+        System.out.println("book2.year = " + book2.getYear());
+        book2.setYear(1989);
+        System.out.println("book2.year = " + book2.getYear());
     }
 }
